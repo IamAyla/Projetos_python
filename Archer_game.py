674d21c05c121b6682b2fdd661b9df8cd6959818
Ayla Morais <107047181+IamAyla@users.noreply.github.com>
2022-07-12 14:25:33 -0300

    Update Archer_game.py

--- Archer_game.py
+++ Archer_game.py
@@ -1,17 +1,21 @@
+#Definindo a classe
 class Archer:
   arrows = 10
   points = 0
   empty = False
+#Definindo os parâmetros para o tiro
   def shoot(self):
     if self.arrows > 0:
       print('Aim... and shoot!')
       self.arrows -= 1
     else:
       self.empty = True
-
+      
+#Definindo pontuação para acerto
   def bullseye(self):
     self.points += 3
-
+    
+#Definindo a recarga
   def reload(self):
     if self.empty == True:
         self.arrows = 10
