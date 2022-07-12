e85d0fd4491334e39b7fb978ac7dc111a66f9728
Ayla Morais <107047181+IamAyla@users.noreply.github.com>
2022-07-12 14:28:14 -0300

    Update Script_NPC_RPG.py

--- Script_NPC_RPG.py
+++ Script_NPC_RPG.py
@@ -1,18 +1,22 @@
+#Atribuindo a classe jogador
 class Player:
   def __init__(self,name):
         self.name = name
-  
+
+#Atribuindo a classe computador, considerando o jogador
 class Computer(Player):
   def __init__(self):
     super().__init__('NPC')
-
+    
+#Definindo a resposta do computador com o input do nome
   def respond(self,player):
     print('Hello,', player.name, 'I am', self.name)
-
+    
+#Atribuindo a classe usuário, considerando o jogad or
 class User(Player):
   def __init__(self, name, level):
     super().__init__(name)
-
+#Definindo a saudação
   def greet(self):
     print('Hi! What is your name?')
 
